52af5b13f48dbd7e35ea0bfe6ac67d92e2f27b31
sakshi-k2002 <sakshikhandale24@gmail.com>
2026-03-14 12:33:42 +0530

    Create ConcentricSquare.java

---
+++ ConcentricSquare.java
@@ -0,0 +1,25 @@
+package codingPractice;
+
+public class ConcentricSquare {
+
+	public static void main(String[] args) {
+		int n = 4;
+		int size = 2 * n - 1;
+
+		for (int i = 0; i < size; i++) {
+
+			for (int j = 0; j < size; j++) {
+
+				int top = i;
+				int left = j;
+				int bottom = size - 1 - i;
+				int right = size - 1 - j;
+
+				int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+				System.out.print((n - min) + " ");
+			}
+
+			System.out.println();
+		}
+	}
