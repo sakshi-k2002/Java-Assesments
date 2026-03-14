eb194c781a53dfce538b3a8c4f18e1bd1d456b26
sakshi-k2002 <sakshikhandale24@gmail.com>
2026-03-14 12:26:09 +0530

    Create NumberPyramid.java

---
+++ NumberPyramid.java
@@ -0,0 +1,29 @@
+package codingPractice;
+
+public class NumberPyramid {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+
+			// print spaces
+			for (int j = i; j < n; j++) {
+				System.out.print("  ");
+			}
+
+			// ascending numbers
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+
+			// descending numbers
+			for (int j = i - 1; j >= 1; j--) {
+				System.out.print(j + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}
